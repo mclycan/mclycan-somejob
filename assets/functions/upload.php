8569a45bcf25524250031f4adf85de834af125ac
mclycan <mclycan@163.com>
2016-01-12 19:46:04 +0800

    1-12-19-46

--- assets/functions/upload.php
+++ assets/functions/upload.php
@@ -1,6 +1,35 @@
 <?php
+session_start ();
 include_once("fileSystem.php");
 include_once("database.php");
+
+echo <<<END
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport"    content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author"      content="Mclycan">
+  
+  <title>Info - Brute Pool</title>
+
+  <link rel="shortcut icon" href="../images/111.png">
+  
+  <!-- Bootstrap itself -->
+  <link href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+
+  <!-- Custom styles -->
+  <link rel="stylesheet" href="../css/magister.css">
+
+  <!-- Fonts -->
+  <link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+  <link href='http://fonts.googleapis.com/css?family=Wire+One' rel='stylesheet' type='text/css'>
+</head>
+
+  <body class="text-shadows">
+END;
+
 	if (empty($_POST)){
 		exit("error 1");
 	}
@@ -29,4 +58,11 @@ include_once("database.php");
 	}
 	//断开数据库
 	closeConnection();
+
+echo <<<END
+</body>
+</html>
+
+END;
+
 ?>
