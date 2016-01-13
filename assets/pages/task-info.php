b2bd4a1e04ee079ff99daf7eb954e88db14d4860
mclycan <mclycan@163.com>
2016-01-13 15:12:33 +0800

    1-13-15-12

--- assets/pages/task-info.php
+++ assets/pages/task-info.php
@@ -26,33 +26,22 @@ echo <<<END
 	<link href='http://fonts.googleapis.com/css?family=Wire+One' rel='stylesheet' type='text/css'>
 </head>
 
-<!-- use "theme-invert" class on bright backgrounds, also try "text-shadows" class -->
 <body class="text-shadows">
 
 <div class="container">
 		<div class="row">
 			<div class="col-md-10 col-lg-10 col-md-offset-1 col-lg-offset-1 text-center">	
 				<div style="padding: 50px ;">
-				<h2 class="title">TASK INFO</h2>
+				<h2>TASK INFO</h2>
 				</div>
-<!--  ========================================================================   -->
-<!--  +++++++++++++++++++++++++++DO SOMTHING HERE+++++++++++++++++++++++++++++   -->
-<form name="form1" method="post" action="task-info.html">
+
+<form name="form1" method="post" action="task-info.php">
 <input type="submit" name="submit" class="btn btn-default btn-lg" value="REFRESH"/>
 </form>
 
-
-<!--  ========================================================================   -->
 			</div> 
-		</div> 
-</div>
+		</div>
 
-<!-- Load js libs only when the page is loaded. -->
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
-<script src="assets/js/modernizr.custom.72241.js"></script>
-<!-- Custom template scripts -->
-<script src="assets/js/magister.js"></script>
 
 END;
 
@@ -104,6 +93,7 @@ END;
 	closeConnection();
 
 echo <<<END
+</div>
 </body>
 </html>
 END; 
