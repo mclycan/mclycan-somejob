ba5aadf68012fd98b31f017055cae5e64aa9e78e
mclycan <mclycan@163.com>
2016-01-13 22:13:04 +0800

    1-13-22-13

--- assets/functions/upload.php
+++ assets/functions/upload.php
@@ -56,7 +56,25 @@ END;
 		
 
 	}else{
-		exit($message);	
+
+echo <<<END
+	<section class="section" id="head">
+		<div class="container">
+
+			<div class="row">
+			 	<div class="col-md-10 col-lg-10 col-md-offset-1 col-lg-offset-1 text-center">	
+
+					<h2 class="subtitle">$message</h2>
+	
+			  	</div>  
+			</div> 
+	
+		</div>
+	</section>
+	exit();	
+
+END;
+
 	}
 	//断开数据库
 	closeConnection();
